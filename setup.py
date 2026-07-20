33681555cbb23b382e46ace3418a699396361ce2
Aayush shah <23053738@kiit.ac.in>
2026-07-20 14:43:22 +0530

    added the cookiecuter templet

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A chrome plugin to detect sentiment of the youtube comment',
+    author='aayush-shah',
+    license='',
+)
